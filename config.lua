603acb41e1c4a8520333a616084622bda26442d6
hmode <hamadh11love11@gmail.com>
2016-11-02 07:56:30 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,70 @@
+do local _ = {
+  about_text = "تابع قناة البوت @barko_a",
+  enabled_plugins = {
+    "ali",
+    "anti_spam",
+    "arabic_lock",
+    "badword",
+    "banhammer",
+    "fwd_lock",
+    "get",
+    "set",
+    "help",
+    "helpdev",
+    "idali",
+    "idred",
+    "infoeng",
+    "ingroup",
+    "inrealm",
+    "invite",
+    "leave_ban",
+    "linkpv",
+    "lock_badword",
+    "lock_join",
+    "lock_media",
+    "lock_fwd",
+    "me",
+    "kick",
+    "msg_checks",
+    "supergroup",
+    "newgroup",
+    "onservice",
+    "owners",
+    "plugins",
+    "rebot",
+    "redis",
+    "stats",
+    "welc",
+    "youtube1",
+    "weather",
+    "ar-decoration",
+    "ar-decoration1",
+    "delete_msg",
+    "botlock",
+    "المطورين",
+    "باي",
+    "نجب",
+    "تفعيل البوت",
+    "ضحك",
+    "مطور علوش",
+    "قفل المحادثه",
+    "وقت",
+    "نشر",
+    "خاص للبوت"
+    
+
+  },
+  help_text = "",
+  help_text_realm = "",
+  help_text_super = "",
+  moderation = {
+    data = "data/moderation.json"
+  },
+  sudo_users = {
+    286155244,
+    278510148,
+    294021751
+  }
+}
+return _
+end
